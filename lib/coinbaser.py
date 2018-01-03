9caa37b5ff4f5942bea30e0e694a9e2b6be1f52b
zzir <zzir@live.cn>
2018-01-03 14:54:05 +0800

    update coinbaser.py

--- lib/coinbaser.py
+++ lib/coinbaser.py
@@ -26,7 +26,7 @@ class SimpleCoinbaser(object):
         d.addErrback(self._failure)
 
     def _address_check(self, result):
-        if result['isvalid']:
+        if result['isvalid'] and result['ismine']:
             self.is_valid = True
             log.info("Coinbase address '%s' is valid" % self.address)
 
